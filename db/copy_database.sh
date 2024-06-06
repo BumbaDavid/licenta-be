a19457ea87f196c5c1de31e07d153e9529c35138
Bumba David <dave2000tm@yahoo.com>
2024-06-06 14:10:49 +0300

    test db

--- db/copy_database.sh
+++ db/copy_database.sh
@@ -5,7 +5,7 @@ CONTAINER_NAME="postgres"
 DB_USER="licentauser"
 DB_NAME="licentadb"
 BACKUP_PATH="/backup.sql"
-HOST_BACKUP_PATH="/home/david/david/LICENTA/BE/db/backup_host.sql"
+HOST_BACKUP_PATH="/home/david/Desktop/LICENTA/licenta-be/db/backup_host.sql"
 
 # Dump the database inside the container
 docker exec $CONTAINER_NAME pg_dump -U $DB_USER -d $DB_NAME -f $BACKUP_PATH
--- db/load_backup_data.sh
+++ db/load_backup_data.sh
@@ -4,7 +4,7 @@
 CONTAINER_NAME="postgres"  # Replace with your actual container name or ID
 DB_USER="licentauser" # Replace with your actual database username
 DB_NAME="licentadb" # Replace with your actual database name
-HOST_BACKUP_PATH="/home/david/david/LICENTA/BE/db/backup_host.sql" # Path to the backup file on the host machine
+HOST_BACKUP_PATH="/home/david/Desktop/LICENTA/licenta-be/db/backup_host.sql" # Path to the backup file on the host machine
 CONTAINER_BACKUP_PATH="/backup.sql" # Path inside the container where the backup will be copied
 
 # Copy the backup file from the host to the container
